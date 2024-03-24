418146480cc169f3e45f6919680f416236476e59
Rodrigo Sturm <rodristurm@gmail.com>
2024-03-23 22:49:33 -0300

    Update: Applied NMS function for avoiding overlaping detections on a single object, plus added a way to draw the bbox of a single class object

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
 import cv2
 import numpy as np
-from util import get_outputs, draw
+from util import get_outputs, draw, NMS
 
 # define constants
 model_cfg_path = os.path.join('.', 'model', 'cfg', 'yolov3.cfg')
 model_weights_path = os.path.join('.', 'model', 'weights', 'yolov3.weights')
 class_names_path = os.path.join('.', 'model', 'class.names')
 
-img_path =  os.path.join('.', 'assets', 'messi_ball.jpg')
+img_path =  os.path.join('.', 'assets', 'messi_ball_2.jpg')
 
 # open class.names, read each line and save it into list class_names
 with open(class_names_path, 'r') as file:
@@ -42,16 +42,44 @@ for detection in detections:
   bbox = [int(xc * W), int(yc * H), int(w * W), int(h * H)]
 
   bbox_score = detection[4]
-  class_id = np.argmax(detection[5:]) # save the INDEX of the heigher detection
+  class_id = np.argmax(detection[5:]) # save the INDEX of the heigher detection; the index count of detection[5:] will start at 0, not at 5, so if the first detection is a person, it will be saved a 0, which is the index from 'detection[5:]'
   score = np.amax(detection[5:]) # save the VALUE of the heigher detection
-  # append all the collected data to the global lists
+
+  # append all the collected data to the general lists
   bboxes.append(bbox)
   class_ids.append(class_id)
   scores.append(score)
+
+# aply NMS; subtract the unnecessary detections for the same object
+bboxes, class_ids, scores = NMS(bboxes, class_ids, scores)
+
+# plot the image with bbox and name object
+for bbox_, bbox in enumerate(bboxes):
+  name_position = class_ids[bbox_]
+  name = class_names[name_position]
+  print(name)
+
+  xc, yc, w, h = bbox
+
+  img = cv2.rectangle(img, 
+                      (int(xc - (w / 2)),  (int(yc - (h / 2)))), 
+                      (int(xc + (w / 2)),  (int(yc + (h / 2)))), 
+                      (255, 0, 0), 3)
   
-# plotting the imgage with the results 
-for bbox in bboxes:
-  img = draw(bbox, img)
+  img = cv2.putText(img, '{}'.format(name),  
+                    (int(xc - (w / 2)),  int(yc - (h / 2)) - 15), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 
+                    0.7, (58, 193, 242), 2)
+
+# plotting the imgage with the results
+# for bbox_, bbox in enumerate(bboxes):
+  # getting the names of the objects detected
+  # position_class_id = class_ids[bbox_]
+  # object_name = class_names[position_class_id]
+
+  # if(object_name =="sports ball"):
+  #   print("yes")
+  #   img = draw(bbox, img)
 
 cv2.imshow('img', img)
 cv2.waitKey(0)
